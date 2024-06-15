036520a47c3f0ed2f0200e303241a02d20bcda57
Matias Salles <matiassalles99@gmail.com>
2024-06-15 19:06:26 -0300

    fix url

--- Formula/memoh.rb
+++ Formula/memoh.rb
@@ -1,7 +1,7 @@
 class Memoh < Formula
   desc "Save your SSH connection details with memoh"
   homepage "https://github.com/matiassalles99/memoh"
-  url "https://rubygems.org/gems/memoh"
+  url "https://rubygems.org/downloads/memoh-1.0.gem"
   sha256 "9c406e8cb0e69f535b59a28f2031330b44b989239015c2c804df1297caaed461"
   license "MIT"
   version "1.0"
